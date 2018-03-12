510a91fadc428e59bf0e65a7534329486ca8b1ff
Hariharan Iyer <hiyer@qubole.com>
2018-03-12 09:50:07 +0530

    fixes

--- hadoop/util.sh
+++ hadoop/util.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 source /usr/lib/hustler/bin/qubole-bash-lib.sh
+export PROFILE_FILE=${PROFILE_FILE:-/etc/profile}
+export HADOOP_ETC_DIR=${HADOOP_ETC_DIR:-/etc/hadoop}
 
 function restart_master_services() {
   monit restart resourcemanager
@@ -14,12 +16,12 @@ function restart_worker_services() {
 function use_java8() {
  export JAVA_HOME=/usr/lib/jvm/java-1.8.0_60
  export PATH=$JAVA_HOME/bin:$PATH
- echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0_60" >> /etc/profile
- echo "export PATH=$JAVA_HOME/bin:$PATH" >> /etc/profile
+ echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0_60" >> "$PROFILE_FILE"
+ echo "export PATH=$JAVA_HOME/bin:$PATH" >> "$PROFILE_FILE"
  
- sed -i 's/java-1.7.0/java-1.8.0_60/' /etc/hadoop/hadoop-env.sh
- sed -i 's/java-1.7.0/java-1.8.0_60/' /etc/hadoop/mapred-env.sh
- sed -i 's/java-1.7.0/java-1.8.0_60/' /etc/hadoop/yarn-env.sh
+ sed -i 's/java-1.7.0/java-1.8.0_60/' "$HADOOP_ETC_DIR/hadoop-env.sh"
+ sed -i 's/java-1.7.0/java-1.8.0_60/' "$HADOOP_ETC_DIR/mapred-env.sh"
+ sed -i 's/java-1.7.0/java-1.8.0_60/' "$HADOOP_ETC_DIR/yarn-env.sh"
 
  is_master=$(nodeinfo is_master)
  if [[ "$is_master" == "1" ]]; then
