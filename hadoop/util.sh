7e2fbec608f1f8450a20d50fbcdde68bfcf83a0b
Hariharan Iyer <hiyer@qubole.com>
2018-06-15 14:47:30 +0530

    Use generic Java 8 version

--- hadoop/util.sh
+++ hadoop/util.sh
@@ -2,7 +2,7 @@
 
 source /usr/lib/hustler/bin/qubole-bash-lib.sh
 export PROFILE_FILE=${PROFILE_FILE:-/etc/profile}
-export HADOOP_ETC_DIR=${HADOOP_ETC_DIR:-/etc/hadoop}
+export HADOOP_ETC_DIR=${HADOOP_ETC_DIR:-/usr/lib/hadoop2/etc/hadoop}
 
 function restart_master_services() {
   monit restart resourcemanager
@@ -11,17 +11,19 @@ function restart_master_services() {
 
 function restart_worker_services() {
   monit restart datanode
+  # No need to restart nodemanager since it starts only
+  # after thhe bootstrap is finished
 }
 
 function use_java8() {
- export JAVA_HOME=/usr/lib/jvm/java-1.8.0_60
+ export JAVA_HOME=/usr/lib/jvm/java-1.8.0
  export PATH=$JAVA_HOME/bin:$PATH
- echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0_60" >> "$PROFILE_FILE"
+ echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0" >> "$PROFILE_FILE"
  echo "export PATH=$JAVA_HOME/bin:$PATH" >> "$PROFILE_FILE"
  
- sed -i 's/java-1.7.0/java-1.8.0_60/' "$HADOOP_ETC_DIR/hadoop-env.sh"
- sed -i 's/java-1.7.0/java-1.8.0_60/' "$HADOOP_ETC_DIR/mapred-env.sh"
- sed -i 's/java-1.7.0/java-1.8.0_60/' "$HADOOP_ETC_DIR/yarn-env.sh"
+ sed -i 's/java-1.7.0/java-1.8.0/' "$HADOOP_ETC_DIR/hadoop-env.sh"
+ sed -i 's/java-1.7.0/java-1.8.0/' "$HADOOP_ETC_DIR/mapred-env.sh"
+ sed -i 's/java-1.7.0/java-1.8.0/' "$HADOOP_ETC_DIR/yarn-env.sh"
 
  is_master=$(nodeinfo is_master)
  if [[ "$is_master" == "1" ]]; then
