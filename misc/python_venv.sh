50d24d1886feeb8a77cf712d3b8ecebfd9969e73
Hariharan Iyer <hiyer@qubole.com>
2018-07-02 15:47:45 +0530

    add comment on how to use requirements.txt

--- misc/python_venv.sh
+++ misc/python_venv.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -x
 
+#
+# This function activates the new virtualenv, so install
+# any libraries you want after calling this with "pip install"
+#
+# Alternatively you can also use a requirements file. For example
+# to use a requirements file stored in S3 or Azure Blob Store, run
+#
+#    /usr/lib/hadoop2/bin/hadoop dfs -get {s3|wasb}://path/to/requirements/file /tmp/requirements.txt
+#    pip install -r /tmp/requirements.txt
+#
+
 function install_python_venv() {
   version=${$1:-36}
   location=${$2:-/usr/lib/virtualenv/py36}
@@ -9,6 +20,8 @@ function install_python_venv() {
 
   virtualenv -p "/usr/bin/python${version}" $location
   hadoop_use_venv "$location"
+
+  source ${location}/bin/activate
 }
 
 function hadoop_use_venv() {
