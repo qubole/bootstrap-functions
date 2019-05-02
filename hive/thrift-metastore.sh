d3df20bc9824bd3845968471f91faf1eec65f7b2
Aditya Karanjkar <akaranjkar@qubole.com>
2019-05-02 10:44:23 -0700

    HIVE-4411

--- hive/thrift-metastore.sh
+++ hive/thrift-metastore.sh
@@ -2,13 +2,33 @@
 
 source /usr/lib/hustler/bin/qubole-bash-lib.sh
 
+is_master=$(nodeinfo is_master)
+use_hadoop2=$(nodeinfo use_hadoop2)
+use_thrift_metastore1_2=$(nodeinfo use_thrift_metastore1_2)
+hive_version=$(nodeinfo hive_version)
+
+if [[ $is_master == "1" ]]; then
+    thrift_metastore_bin_path=/usr/lib/hive13/bin
+    if [[ $use_hadoop2 == "1" ]]; then
+        thrift_metastore_bin_path=/usr/lib/hive2/bin
+        if [[ $use_thrift_metastore1_2 == "true" ]]; then
+            thrift_metastore_bin_path=/usr/lib/hive1.2/bin
+        fi
+    fi
+    if [[ "$hive_version" == "3.1.1" ]]; then
+        thrift_metastore_bin_path=/usr/lib/hive1.2/bin
+    fi
+fi
+
+
 ##
 # Start thrift metastore server
 #
 function start_thrift_metastore() {
-    is_master=$(nodeinfo is_master)
     if [[ "$is_master" == "1" ]]; then
-        /usr/lib/hive1.2/bin/thrift-metastore server start
+        ${thrift_metastore_bin_path}/thrift-metastore server start
+    else
+        return 1
     fi
 }
 
@@ -16,9 +36,10 @@ function start_thrift_metastore() {
 # Stop thrift metastore server
 #
 function stop_thrift_metastore() {
-    is_master=$(nodeinfo is_master)
     if [[ "$is_master" == "1" ]]; then
-        /usr/lib/hive1.2/bin/thrift-metastore server stop
+        ${thrift_metastore_bin_path}/thrift-metastore server stop
+    else
+        return 1
     fi
 }
 
