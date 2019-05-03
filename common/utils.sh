798cb6278311b5f65c682a71a2b77c34ec46877d
Himanshu Mishra <himanshum@qubole.com>
2019-05-03 12:35:37 +0530

    Fixing return values from methods

--- common/utils.sh
+++ common/utils.sh
@@ -12,30 +12,30 @@ populate_nodeinfo() {
 # Returns 0 when run on a Hadoop2 cluster node.
 # Returns 1 otherwise
 is_hadoop2_cluster() {
-   [ `nodeinfo use_hadoop2` = "1" ]
+   [[ `nodeinfo use_hadoop2` = "1" ]]
 }
 
 
 # Returns 0 when HiveServer2 is configured to run on the cluster master.
 # Returns 1 otherwise
 is_hs2_enabled() {
-  [[ is_hadoop2_cluster && `nodeinfo use_hs2` = "1" ]]
+  is_hadoop2_cluster && [[ `nodeinfo use_hs2` = "1" ]]
 }
 
 # Returns 0 when run on a HiveServer2 cluster node.
 # Returns 1 otherwise
 is_hs2_cluster() {
-  [[ is_hadoop2_cluster && `nodeinfo is_hs2_cluster` = "1" ]]
+  is_hadoop2_cluster && [[ `nodeinfo is_hs2_cluster` = "1" ]]
 }
 
 # Returns 0 when run on a cluster master node.
 # Returns 1 otherwise
 is_master_node() {
-   [ `nodeinfo is_master` = "1" ]
+   [[ `nodeinfo is_master` = "1" ]]
 }
 
 # Returns 0 when run on a cluster worker node.
 # Returns 1 otherwise
 is_worker_node() {
-   [ ! is_master_node  ]
+   ! is_master_node
 }
--- hive/hiveserver2.sh
+++ hive/hiveserver2.sh
@@ -7,7 +7,7 @@ source /usr/lib/qubole/bootstrap-functions/common/utils.sh
 #--------------------------------------------------------------------------------
 
 is_hs2_configured() {
-  [[ (is_master_node && is_hs2_enabled) || (is_worker_node && is_hs2_cluster) ]]
+  (is_master_node && is_hs2_enabled) || (is_worker_node && is_hs2_cluster)
 }
 
 # Stop HiveServer2 JVM - works on both Hadoop2 and HiveServer2 cluster
