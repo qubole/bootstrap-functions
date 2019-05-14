c9501bd8774d7acd9efd506fe98454f3f362a707
Aditya Karanjkar <akaranjkar@qubole.com>
2019-05-13 17:27:01 -0700

    Use monit instead of bin path

--- hive/thrift-metastore.sh
+++ hive/thrift-metastore.sh
@@ -3,30 +3,13 @@
 source /usr/lib/hustler/bin/qubole-bash-lib.sh
 
 is_master=$(nodeinfo is_master)
-use_hadoop2=$(nodeinfo use_hadoop2)
-use_thrift_metastore1_2=$(nodeinfo use_thrift_metastore1_2)
-hive_version=$(nodeinfo hive_version)
-
-if [[ $is_master == "1" ]]; then
-    thrift_metastore_bin_path=/usr/lib/hive13/bin
-    if [[ $use_hadoop2 == "1" ]]; then
-        thrift_metastore_bin_path=/usr/lib/hive2/bin
-        if [[ $use_thrift_metastore1_2 == "true" ]]; then
-            thrift_metastore_bin_path=/usr/lib/hive1.2/bin
-        fi
-    fi
-    if [[ "$hive_version" == "3.1.1" ]]; then
-        thrift_metastore_bin_path=/usr/lib/hive1.2/bin
-    fi
-fi
-
 
 ##
 # Start thrift metastore server
 #
 function start_thrift_metastore() {
     if [[ "$is_master" == "1" ]]; then
-        ${thrift_metastore_bin_path}/thrift-metastore server start
+        monit start metastore1_2
     else
         return 1
     fi
@@ -37,7 +20,7 @@ function start_thrift_metastore() {
 #
 function stop_thrift_metastore() {
     if [[ "$is_master" == "1" ]]; then
-        ${thrift_metastore_bin_path}/thrift-metastore server stop
+        monit stop metastore1_2
     else
         return 1
     fi
