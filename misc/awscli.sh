0be82c412598b4e0d97e48434e7fecfb4052fe1a
Aditya Karanjkar <akaranjkar@qubole.com>
2019-07-08 13:49:16 -0700

    Accept credentials file instead

--- misc/awscli.sh
+++ misc/awscli.sh
@@ -3,26 +3,24 @@
 ##
 # Configure AWS CLI
 # -p: Name of the profile. Defaults to `default`
-# -a: AWS access key
-# -s: AWS secret key
 # -r: AWS region
+# -c: Credentials file
+# The credentials file must contain the AWS Access Key and
+# the AWS Secret Key separated by a space, comma, tab or newline
 #
 function configure_awscli() {
     PROFILE=default
 
-    while getopts ":p:a:s:r:" opt; do
+    while getopts ":p:r:c:" opt; do
         case ${opt} in
             p)
                 PROFILE=${OPTARG}
                 ;;
-            a)
-                ACCESS_KEY=${OPTARG}
-                ;;
-            s)
-                SECRET_KEY=${OPTARG}
-                ;;
             r)
                 REGION=${OPTARG}
+                ;;
+            c)
+                CREDENTIALS_FILE=${OPTARG}
                 ;;
             \?)
                 echo "Invalid option: -${OPTARG}" >&2
@@ -39,13 +37,21 @@ function configure_awscli() {
         echo "AWS CLI was not found.."
         return 1
     fi
-    if [[ ! -z ${ACCESS_KEY} ]]; then
-        aws configure set aws_access_key_id ${ACCESS_KEY} --profile ${PROFILE}
-    fi
-    if [[ ! -z ${SECRET_KEY} ]]; then
-        aws configure set aws_secret_access_key ${SECRET_KEY} --profile ${PROFILE}
-    fi
     if [[ ! -z ${REGION} ]]; then
         aws configure set region ${REGION} --profile ${PROFILE}
+    fi
+    if [[ ! -z ${CREDENTIALS_FILE} ]]; then
+        if [[ -f ${CREDENTIALS_FILE} && -r ${CREDENTIALS_FILE} ]]; then
+            OLDIFS="$IFS"
+            IFS=$' |\t|\n|,' CONTENTS=( $(cat ${CREDENTIALS_FILE}) )
+            IFS="$OLDIFS"
+            [[ ${#CONTENTS[@]} -lt 2 ]] && return 1
+            ACCESS_KEY=${CONTENTS[0]}
+            SECRET_KEY=${CONTENTS[1]}
+            aws configure set aws_access_key_id ${ACCESS_KEY} --profile ${PROFILE}
+            aws configure set aws_secret_access_key ${SECRET_KEY} --profile ${PROFILE}
+        else
+            return 1
+        fi
     fi
 }
