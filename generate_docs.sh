d7d5cd1209d5b59fb793de3a6ab1f9e4bbd094db
Aditya Karanjkar <akaranjkar@qubole.com>
2020-03-10 18:06:40 -0700

    Doc generation script

---
+++ generate_docs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Cleanup older documentation
+mkdir -p docs
+rm -f docs/*.md
+
+# Generate new documentation
+directories=$(ls -d */ | grep -v "docs\|tests\|examples")
+for dx in ${directories}; do
+    find ${dx} -type f -name "*.sh" -exec shdoc {} \; > docs/$(dirname ${dx}.).md
+done
+
+# Overwrite README.md
+cp -f README.template README.md
+for dx in ${directories}; do
+    d=$(dirname ${dx}.)
+    sed -i "/The following set of functions are available at present:/a * [${d}] (docs/${d}.md)" README.md
+done
