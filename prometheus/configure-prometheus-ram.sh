f8be07b4c398902b16ebd7fbdf2a3730f562845f
agarg <agarg@qubole.com>
2020-05-02 06:15:05 +0530

    comments added

--- prometheus/configure-prometheus-ram.sh
+++ prometheus/configure-prometheus-ram.sh
@@ -9,26 +9,32 @@
 # @example
 #      function requires one argument to be passed.
 #      Argument must specify the ram to be allocated to the prometheus service from master node ram.
-#
-#      Suffixes :-
-#      1) Add 'M' at the end to denote Megabyte.
-#      2) Add 'G' at the end to denote Gigabyte
+#      Input should be an integer. All the values are assumed in MB.
 #
 #      Example of function calling :-
-#      1) configure_prometheus_ram_on_master '600M'
-#      2) configure_prometheus_ram_on_master 600M
+#      1) configure_prometheus_ram_on_master '600'
+#      2) configure_prometheus_ram_on_master 600
 #      Both of them are acceptable.
 #
 #      sed -i "s/PROMETHEUS_RAM/$1/g" /usr/lib/prometheus/config/docker-compose.yml
 #
-#
+
 function configure_prometheus_ram_on_master() {
+
   prometheus_ram=$1
+  # Regex to determine whether the input is a number or not.
   regex_number='^[0-9]+$'
+
+  # Add suffix 'M' to the user's input.
   prometheus_ram_with_suffix="${prometheus_ram}M"
+
   if [[ -n $prometheus_ram ]] && [[ $prometheus_ram =~ $regex_number ]]; then
+
+    # If the user input is non-empty and is matching with the given regex, then we can override the value.
     sed -i "s/PROMETHEUS_RAM/$prometheus_ram_with_suffix/g" /usr/lib/prometheus/config/docker-compose.yml
+
   fi
+
 }
 
 
