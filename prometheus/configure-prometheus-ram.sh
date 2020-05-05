d277a62a253a43c7b73729eae0eef03d6c5e0de1
agarg <agarg@qubole.com>
2020-05-05 23:14:44 +0530

    changes in commenting

--- prometheus/configure-prometheus-ram.sh
+++ prometheus/configure-prometheus-ram.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 #
 # @file prometheus/configure-prometheus-ram.sh
-# @brief Provides functions to Configure Prometheus memory usage on master node
-
-
-# @description Ability to specify the memory usage of prometheus daemon on master. Example : 500M
+# @brief Provides functions to configure Prometheus
+#
+#
+# @description Ability to override the memory usage of prometheus daemon on master. Example : 500M
+#
+#  function requires one argument to be passed.
+#  Argument must specify the ram to be allocated to the prometheus service from master node ram.
+#  Input should be an integer. All the values are assumed in MB.
 #
 # @example
-#      function requires one argument to be passed.
-#      Argument must specify the ram to be allocated to the prometheus service from master node ram.
-#      Input should be an integer. All the values are assumed in MB.
+#      configure_prometheus_ram_on_master 600
 #
-#      Example of function calling :-
-#      1) configure_prometheus_ram_on_master '600'
-#      2) configure_prometheus_ram_on_master 600
-#      Both of them are acceptable.
+# @arg $1 integer Prometheus ram to be substituted in MB.
 #
-#      sed -i "s/PROMETHEUS_RAM/$1/g" /usr/lib/prometheus/config/docker-compose.yml
 #
 
 function configure_prometheus_ram_on_master() {
@@ -36,5 +34,3 @@ function configure_prometheus_ram_on_master() {
   fi
 
 }
-
-
