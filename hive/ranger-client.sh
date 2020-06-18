adac0296cc9c49a17121d408885afc78715b92b5
Pritish Sahoo <pritishsahoo>
2020-06-18 08:41:02 -0500

    Removed Special characters

--- hive/ranger-client.sh
+++ hive/ranger-client.sh
@@ -2,10 +2,10 @@
 #
 # @file hive/ranger-client.sh
 # @brief Provides function to install Apache Ranger client for Hive
-​
+
 source /usr/lib/qubole/bootstrap-functions/common/utils.sh
 source /usr/lib/qubole/bootstrap-functions/hive/hiveserver2.sh
-​
+
 # @description Install Apache Ranger client for Hive
 #
 # Currently supported only on AWS
@@ -19,7 +19,7 @@ source /usr/lib/qubole/bootstrap-functions/hive/hiveserver2.sh
 # @arg -r string Name of Ranger repository. Defaults to `hivedev`
 # @arg -S string Hostname of Solr admin. Defaults to `""`
 # @arg -P int Port where Solr admin is running. Defaults to `6083`
-​
+
 function install_ranger() {
     populate_nodeinfo
     if [[ is_hs2_configured ]]; then
@@ -36,7 +36,7 @@ function install_ranger() {
         elif [ "${HIVE_VERSION}" = "3.1.1" ]; then
             PLUGIN_VER="2.0.0"
         fi
-​
+
         while getopts ":h:p:r:S:P:" opt; do
             case ${opt} in
                 h)
@@ -64,60 +64,60 @@ function install_ranger() {
                     ;;
             esac
         done
-​
+
         URL=http://${HOST}:${PORT}
         HIVE_LIB=/usr/lib/hive1.2
-​
+
         S3_PATH=s3://paid-qubole/ranger-${PLUGIN_VER}
         RANGER_PLUGIN=ranger-${PLUGIN_VER}-hive-plugin
         RANGER_PLUGIN_FILE=${S3_PATH}/${RANGER_PLUGIN}.tar.gz
         SPOOL_DIR=/media/ephemeral0/logs/ranger/hive/audit/solr
-​
+
         change_java_version "1.8" "1.8"
         mkdir -p /media/ephemeral0/hive_plugin
         pushd /media/ephemeral0/hive_plugin
-​
+
         # Fetch and extract Ranger Hive plugin
         /usr/lib/hadoop2/bin/hadoop dfs -get ${RANGER_PLUGIN_FILE}
         tar -xzf ${RANGER_PLUGIN}.tar.gz
         cd ${RANGER_PLUGIN}
         /usr/lib/hadoop2/bin/hadoop dfs -get ${S3_PATH}/hc_user.sh
-​
+
         chown -R ${USER}:${USER} .
         chmod 755 *.sh *.py
-​
+
         # Configure install.properties
         sed -i "s#^\(REPOSITORY_NAME=\).*#REPOSITORY_NAME=${REPOSITORY_NAME}#g" install.properties
         sed -i "s#^\(POLICY_MGR_URL=\).*#POLICY_MGR_URL=${URL}#g" install.properties
         sed -i "s#^\(COMPONENT_INSTALL_DIR_NAME=\).*#COMPONENT_INSTALL_DIR_NAME=${HIVE_LIB}#g" install.properties
         sed -i "s#^\(HCOMPONENT_INSTALL_DIR=\).*#HCOMPONENT_INSTALL_DIR=${HIVE_LIB}#g" enable-hive-plugin.sh
         sed -i "s#POLICY_CACHE_FILE_PATH=/etc#POLICY_CACHE_FILE_PATH=/media/ephemeral0/hive_plugin/data#g" enable-hive-plugin.sh
-​
+
         # Enable Solr Configure install.properties
         if [[ $SOLR_HOST -ne "" ]]; then
             SOLR_URL=http://${SOLR_HOST}:${SOLR_PORT}/solr/ranger_audits
-​
+
             sed -i "s#\(XAAUDIT.SOLR.ENABLE=\).*#XAAUDIT.SOLR.ENABLE=true#g" install.properties
             sed -i "s#\(XAAUDIT.SOLR.URL=\).*#XAAUDIT.SOLR.URL=$SOLR_URL#g" install.properties
             sed -i "s#\(XAAUDIT.SOLR.IS_ENABLED=\).*#XAAUDIT.SOLR.IS_ENABLED=true#g" install.properties
             sed -i "s#\(XAAUDIT.SOLR.SOLR_URL=\).*#XAAUDIT.SOLR.SOLR_URL=$SOLR_URL#g" install.properties
             sed -i "s#\(XAAUDIT.SOLR.FILE_SPOOL_DIR=\).*#XAAUDIT.SOLR.FILE_SPOOL_DIR=${SPOOL_DIR}#g" install.properties
         fi
-​
+
         # Run enable-hive-plugin.sh
         ./enable-hive-plugin.sh
         chmod -R 777 /media/ephemeral0/hive_plugin/data
-​
+
         # Update Qubole Health check user
         ./hc_user.sh /usr/lib/hive1.2/bin/test-hs2.sh ${QBOL_HEALTH_CHECK_USER}
-​
+
         # Copy Ranger files in the expected path
         cp -r /usr/lib/hive1.2/lib/ranger-hive-plugin-impl/* /usr/lib/hive1.2/lib/
-​
+
         # Restart HS2
         echo "Restarting HS2"
         restart_hs2
-​
+
         popd
         return 0
     else
